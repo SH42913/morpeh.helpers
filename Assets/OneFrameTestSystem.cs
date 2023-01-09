b790bf58dc31f1dd8cad9c396fd4c131d463ba27
SH42913 <Shoot913@gmail.com>
2023-01-09 18:21:28 +0400

    OneFrameTestSystem

---
+++ Assets/OneFrameTestSystem.cs
@@ -0,0 +1,30 @@
+﻿using Scellecs.Morpeh;
+using Scellecs.Morpeh.Helpers.OneFrame;
+using UnityEngine;
+
+[Unity.IL2CPP.CompilerServices.Il2CppSetOption(Unity.IL2CPP.CompilerServices.Option.NullChecks, false)]
+[Unity.IL2CPP.CompilerServices.Il2CppSetOption(Unity.IL2CPP.CompilerServices.Option.ArrayBoundsChecks, false)]
+[Unity.IL2CPP.CompilerServices.Il2CppSetOption(Unity.IL2CPP.CompilerServices.Option.DivideByZeroChecks, false)]
+[CreateAssetMenu(menuName = "ECS/" + nameof(OneFrameTestSystem))]
+public sealed class OneFrameTestSystem : Scellecs.Morpeh.Systems.UpdateSystem {
+    [SerializeField] private OneFrameRegister oneFrameRegister;
+
+    private Filter oneFrameFilter;
+
+    public override void OnAwake() {
+        oneFrameRegister.RegisterOneFrame<OneFrameComponent>(World);
+        oneFrameFilter = World.Filter.With<OneFrameComponent>();
+    }
+
+    public override void OnUpdate(float deltaTime) {
+        if (oneFrameFilter.IsEmpty()) {
+            World.CreateEntity().AddComponent<OneFrameComponent>();
+        } else {
+            Debug.LogError("There's more than one component! Looks like OneFrame was not clean!");
+        }
+    }
+
+    public static OneFrameTestSystem Create() => CreateInstance<OneFrameTestSystem>();
+
+    private struct OneFrameComponent : IComponent { }
+}
